b8531cd12f9e46bbe9d79f50067485ca85b11b01
Waleed Rahman <wir349@gmail.com>
2017-02-18 12:36:45 -0800

    initial main file

---
+++ main.py
@@ -0,0 +1,11 @@
+# takes code input
+
+def something():
+    print("Hello World")
+    inputText = raw_input("Enter The Code you want to search for:")
+    print(inputText)
+
+def main():
+    something()
+
+main()
