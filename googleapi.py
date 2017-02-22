069289aff7142c29ab69fc24f1f91da243e0f880
Ziye Xing <xingziye46@gmail.com>
2017-02-22 02:48:39 -0800

    add google search api

---
+++ googleapi.py
@@ -0,0 +1,12 @@
+import pprint
+from googleapiclient.discovery import build
+
+API_KEY = "AIzaSyCJLui_HD-THLZkEmBmWvfsj1GLjCx_M9A"
+CSE_ID = "008171140621555576608:krdkbofgvt8"
+
+service = build("customsearch", "v1", developerKey=API_KEY)
+
+res = service.cse().list(q='something', cx=CSE_ID).execute()
+items = res['items']
+for item in items:
+    pprint.pprint(item)
